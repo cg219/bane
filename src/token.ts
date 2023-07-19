c44c60e8985240b389188d027fd014b038544827
Mente <dev@imkreative.com>
2023-07-19 00:38:28 -0400

    Refresh tokens and all that

--- src/token.ts
+++ src/token.ts
@@ -1,12 +1,13 @@
 import * as jwt from "jsonwebtoken";
+import { getSession } from "./session.ts";
 
 const JWT_SECRET = Deno.env.get('JWT_SECRET') ?? 'secret';
 
-export default { createRefreshToken, createAccessToken, createTokens, decodeToken }
+export default { createRefreshToken, createAccessToken, createTokens, decodeToken, refreshTokens }
 
 export function createRefreshToken(sessionUUID: string) {
     try {
-        return jwt.sign({ uuid: sessionUUID}, JWT_SECRET);
+        return jwt.sign({ uuid: sessionUUID }, JWT_SECRET);
     } catch (error) { console.error(error) }
 }
 
@@ -21,6 +22,18 @@ export function createTokens(sessionUUID: string, userID: string) {
         accessToken: createAccessToken(sessionUUID, userID),
         refreshToken: createRefreshToken(sessionUUID)
     }
+}
+
+export async function refreshTokens(refreshToken: string) {
+    try {
+        const { uuid } = decodeToken(refreshToken);
+        const session = await getSession(uuid);
+
+        if (session.valid) {
+            return createTokens(session.uuid, session.userid);
+        }
+
+    } catch(_) { }
 }
 
 export function decodeToken(token: string) {
